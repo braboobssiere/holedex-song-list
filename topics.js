a2b821816631c4b65b39ccb404582540656ba8d0
Boobs <86918103+braboobssiere@users.noreply.github.com>
2024-06-14 03:23:35 +0000

    Create topics.js

---
+++ topics.js
@@ -0,0 +1,10 @@
+var topics = [
+    { value: "singing", label: "Singing" },
+    { value: "asmr", label: "ASMR" },
+    { value: "Anniversary", label: "Anniversary" },
+    { value: "Birthday", label: "Birthday" },
+    { value: "Musical_Instrument", label: "Musical Instrument" },
+    { value: "Original_Song", label: "Original" },
+    { value: "Music_Cover", label: "Cover" },
+    { value: "3D_Stream", label: "3D LIVE" }
+];
