0fbf73b8dcccc3c4b7720eb9becc3d291caff829
Boobs <86918103+braboobssiere@users.noreply.github.com>
2024-07-06 10:18:51 +0000

    Update generate_atom_feed.js

--- generate_atom_feed.js
+++ generate_atom_feed.js
@@ -45,7 +45,7 @@ function createAtomFeed(videos, feedUrl) {
     const englishName = video.channel.english_name;
     const authorUrl = `https://www.youtube.com/channel/${video.channel.id}`;
     const formattedAvailableTime = availableAt.toLocaleString('en-US', timeZoneOptions) + ' GMT+7';
-    const summary = `【LIVE on ${formattedAvailableTime}】${link}`;
+    const summary = `<![CDATA[【LIVE on ${formattedAvailableTime}】${link}]]>`;
     
     feed += `
   <entry>
@@ -84,7 +84,7 @@ function handleResponse(response) {
       const feed = createAtomFeed(videos, feedUrl);
 
       const outputPath = path.join(__dirname, 'feeds', 'holodex.atom');
-      fs.writeFileSync(outputPath, feed);
+      fs.writeFileSync(outputPath, feed, 'utf8');
       console.log('Atom feed generated successfully at', outputPath);
     } else {
       console.log(`Error: ${response.statusCode} ${response.statusMessage}`);
